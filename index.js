ab3285d67978398b5edd0e00d6d0639fd6dce5f4
shawara <m.shawara@outlook.com>
2022-01-13 00:56:42 +0200

    add reddit workchronicles

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 const { parse } = require('node-html-parser')
 const axios = require("axios")
-const { WebClient } = require('@slack/web-api')
+const { uploadFileToSlack } = require("./slack");
 
-
-const SLACK_CHANNELS = process.env.SLACK_CHANNELS
-const TOKEN = process.env.SLACK_TOKEN
 const LAST_POST_TITLE = process.env.LAST_POST
 
 const BASE_URL = 'https://thecodinglove.com'
 
-const client = new WebClient(TOKEN)
-
-const uploadFileToSlack = async (fileUrl, title) => {
-    const file = await axios({ method: 'get', url: fileUrl, responseType: 'stream' })
-
-    await client.files.upload({
-        channels: SLACK_CHANNELS,
-        filename: title,
-        file: file.data
-    });
-}
 
 const getPost = async (random = false) => {
     const postsUrl = random ? `${ BASE_URL }/random` : BASE_URL
